29c233ac703d5681fab0386006098fb8701e54cb
Amelit-Q <amelitstanding@gmail.com>
2020-10-22 19:49:43 +0300

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCZRUEjrhWSLuDOUv1s5MQxXifHynZ_xq8',
+  authDomain: 'whatsapp-clone-a4953.firebaseapp.com',
+  databaseURL: 'https://whatsapp-clone-a4953.firebaseio.com',
+  projectId: 'whatsapp-clone-a4953',
+  storageBucket: 'whatsapp-clone-a4953.appspot.com',
+  messagingSenderId: '929639868432',
+  appId: '1:929639868432:web:c75daa2e5ad4bf1b958295',
+};
