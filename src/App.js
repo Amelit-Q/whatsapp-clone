00c83feb543ee9c356a8d24a644cef2859f58e30
Amelit-Q <amelitstanding@gmail.com>
2020-10-25 14:55:40 +0300

    keep doing this messenger

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import './App.css';
+import { Chat } from './components/Chat';
+import { Sidebar } from './components/Sidebar';
+import './styles/app.scss';
 
 function App() {
   return (
     <div className="App">
-      <div className="app__body"></div>
+      <div className="app__body">
+        <Sidebar />
+        <Chat />
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,33 @@
+import { Avatar, IconButton } from '@material-ui/core';
+import { Chat, DonutLarge, MoreVert, SearchOutlined } from '@material-ui/icons';
+import React from 'react';
+import './../styles/sidebar.scss';
+
+export const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLarge />
+          </IconButton>
+          <IconButton>
+            <Chat />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar__search">
+        <IconButton>
+          <SearchOutlined />
+        </IconButton>
+        4943
+        <input placeholder="Search or start new chat" type="text" />
+      </div>
+      <div className="sidebar__chats"></div>
+    </div>
+  );
+};
